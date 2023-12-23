62cbb9b22f51a27789b5e97fd613ffb8f5699331
Caleb Chan <calebchanwy@gmail.com>
2023-12-23 23:18:01 +0000

    Reorganise scripts and templates into folders

--- tjc-powerpoint/src/scripts/index.js
+++ tjc-powerpoint/src/scripts/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const templatesPath = path.resolve(__dirname,'..', 'templates');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -9,15 +10,15 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(templatesPath,'index.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
