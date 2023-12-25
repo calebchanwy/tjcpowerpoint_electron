92d2177abd55a264eb580f920beb7e7da9261003
Caleb Chan <calebchanwy@gmail.com>
2023-12-25 22:29:55 +0000

    Add basic custom titlebar functionality

--- tjc-powerpoint/src/scripts/index.js
+++ tjc-powerpoint/src/scripts/index.js
@@ -1,6 +1,24 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
-const templatesPath = path.resolve(__dirname,'..', 'templates');
+const fs = require('fs');
+const templatesPath = path.resolve(__dirname, '..', 'templates');
+const assetsPath = path.resolve(__dirname, '..', 'assets');
+
+let mainWindow;
+
+// Listen for the 'minimize-window' message from the renderer process
+ipcMain.on('minimize-window', () => {
+  if (mainWindow) {
+    mainWindow.minimize();
+  }
+});
+
+// Listen for the 'close-window' message from the renderer process
+ipcMain.on('close-window', () => {
+  if (mainWindow) {
+    mainWindow.close();
+  }
+});
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -9,42 +27,71 @@ if (require('electron-squirrel-startup')) {
 
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
+  mainWindow = new BrowserWindow({
+    width: 1080,
+    height: 650,
+    frame: false,
+    icon: path.join(assetsPath, 'tjcbirdlogo.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation:false,
     },
+
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(templatesPath,'index.html'));
+  mainWindow.loadFile(path.join(templatesPath, 'index.html'));
 
-  // Open the DevTools.
+  // Handle window closed event
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  // Load the title bar HTML content and send it to the renderer process
+  const titleBarContent = fs.readFileSync(path.join(templatesPath, 'titleBar.html'), 'utf8');
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.send('load-title-bar', titleBarContent);
+  });
+
   mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// Check if another instance is already running
+const isSingleInstance = app.requestSingleInstanceLock();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+if (!isSingleInstance) {
+  // If another instance is already running, quit the new instance
+  app.quit();
+} else {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow);
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+  // Quit when all windows are closed, except on macOS.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  // Listen for the second instance attempting to run
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // If a second instance is launched, focus on the main window
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
+
+
---
+++ tjc-powerpoint/src/scripts/renderer.js
@@ -0,0 +1,20 @@
+const { ipcRenderer } = require('electron');
+
+// Function to minimize the window
+function minimizeWindow() {
+    ipcRenderer.send('minimize-window');
+  }
+  
+  // Function to close the window
+  function closeWindow() {
+    ipcRenderer.send('close-window');
+  }
+
+// Listen for the 'load-title-bar' message
+ipcRenderer.on('load-title-bar', (event, titleBarContent) => {
+  document.getElementById('title-bar-container').innerHTML = titleBarContent;
+
+  // Attach event listeners to your custom buttons
+  document.getElementById('minimizeBtn').addEventListener('click', minimizeWindow);
+  document.getElementById('closeBtn').addEventListener('click', closeWindow);
+});
